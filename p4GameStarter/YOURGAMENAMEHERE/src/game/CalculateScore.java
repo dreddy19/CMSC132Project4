bb9d81eb115dcef392186f10b075863cfafcd720
dreddy19 <161543686+dreddy19@users.noreply.github.com>
2024-03-03 15:53:44 -0500

    Update CalculateScore.java

--- p4GameStarter/YOURGAMENAMEHERE/src/game/CalculateScore.java
+++ p4GameStarter/YOURGAMENAMEHERE/src/game/CalculateScore.java
@@ -1,5 +1,17 @@
 package game;
 
+/**
+ * CLASS: CalculateScore DESCRIPTION: Represents the calculation strategy for
+ * scoring in the game. This interface defines a method for calculating the
+ * score based on the game's rules or player's achievements.
+ */
 public interface CalculateScore {
+
+	/**
+	 * Calculates the score based on the given input.
+	 * 
+	 * @param score The base score to be calculated upon.
+	 * @return The calculated score after applying the game's scoring rules.
+	 */
 	public int calcScore(int score);
 }
